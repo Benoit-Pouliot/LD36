b33a1d50f5171d7243432445458c3465a4f422e1
Benoit-Pouliot <pouliot.benoit@gmail.com>
2016-08-28 15:23:56 -0400

    FCT: EnemyPhone

---
+++ app/sprites/enemy/enemyPhone.py
@@ -0,0 +1,46 @@
+import pygame
+import os
+import random
+
+from app.sprites.enemy.enemy import Enemy
+from app.sprites.bullet import NoteBulletDiag
+from app.settings import *
+from app.sprites.collisionMask import CollisionMask
+
+class EnemyPhone(Enemy):
+    def __init__(self, x, y, theMap, iterStart=0):
+        super().__init__(x, y, os.path.join('img', 'EnemyPhone.png'))
+
+        self.name = "EnemyPhone"
+
+        self.collisionMask = CollisionMask(self.rect.x, self.rect.y, self.rect.width, self.rect.height)
+
+        self.speedx = 0
+        self.speedy = 0
+
+        self.theMap = theMap
+
+        self.isGravityApplied = False
+        self.isCollisionApplied = False
+
+        self.imageIterShoot = 40
+        self.imageWaitNextShoot = 80
+
+        self.life = 1
+
+    def update(self):
+
+        self.imageIterShoot += 1
+        if self.imageIterShoot > self.imageWaitNextShoot:
+
+            self.imageIterShoot = 0
+
+            random.seed()
+            if random.randint(1, 2) == 1:
+                bullet = NoteBulletDiag(self.rect.x+32, self.rect.y+1, RIGHT, -90, False)
+            else:
+                bullet = NoteBulletDiag(self.rect.x+32+32, self.rect.y+1, LEFT, -90, False)
+
+            self.theMap.camera.add(bullet)
+            self.theMap.allSprites.add(bullet)
+            self.theMap.enemyBullet.add(bullet)
--- app/sprites/enemyFactory.py
+++ app/sprites/enemyFactory.py
@@ -4,6 +4,7 @@ from app.sprites.enemy.enemyRadio import EnemyRadio
 from app.sprites.enemy.enemyMp3 import EnemyMp3
 from app.sprites.enemy.enemyMusicBoss import EnemyMusicBoss
 from app.sprites.enemy.enemyNoteInv import EnemyNoteInv
+from app.sprites.enemy.enemyPhone import EnemyPhone
 from app.tools.functionTools import *
 
 
@@ -23,6 +24,8 @@ class EnemyFactory:
             return self.createEnemyMusicBoss(enemy, theMap)
         if eName == "enemyNoteInv":
             return self.createEnemyNoteInv(enemy, theMap)
+        if eName == "enemyPhone":
+            return self.createEnemyPhone(enemy, theMap)
 
         # Not found? we send a simple enemy
         return Enemy(enemy.x, enemy.y)
@@ -37,6 +40,19 @@ class EnemyFactory:
             return EnemyNoteInv(enemy.x, enemy.y, theMap)
         else:
             return EnemyNoteInv(enemy.x, enemy.y, theMap, int(iterStart))
+
+    def createEnemyPhone(self, enemy, theMap):
+        imageIterShoot = seekAtt(enemy, "firstShoot")
+        imageWaitNextShoot = seekAtt(enemy, "interval")
+
+        enemyPhone = EnemyPhone(enemy.x, enemy.y, theMap)
+
+        if imageIterShoot:
+            enemyPhone.imageIterShoot = int(imageIterShoot)
+        if imageWaitNextShoot:
+            enemyPhone.imageWaitNextShoot = int(imageWaitNextShoot)
+
+        return enemyPhone
 
     def createEnemyMp3(self, enemy, theMap):
 
