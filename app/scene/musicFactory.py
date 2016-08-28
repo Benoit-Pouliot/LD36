b7e59cb898ce5995d3d26030b56105704bdec909
Pascal <dump02@gmail.com>
2016-08-27 20:34:38 -0400

    Update Music MainTheme

--- app/scene/musicFactory.py
+++ app/scene/musicFactory.py
@@ -8,16 +8,17 @@ class MusicFactory:
 
         self.nameMusic = None
         if typeScene == TITLE_SCREEN:
-            self.nameMusic = 'MainTheme'
+            pass
         elif typeScene == WORLD_MAP:
             self.nameMusic = 'MainTheme'
         elif typeScene == PLATFORM_SCREEN:
+            self.nameMusic = 'MainTheme'
             if secondOption == "LevelSheriff":
                 self.nameMusic = 'Sheriff'
 
         if self.nameMusic is not None:
             pygame.mixer.music.load(os.path.join('music_pcm', self.nameMusic + '.wav'))
-            pygame.mixer.music.set_volume(1.0)
+            pygame.mixer.music.set_volume(0.5)
             pygame.mixer.music.play(-1)
         else:
             pygame.mixer.music.stop()
