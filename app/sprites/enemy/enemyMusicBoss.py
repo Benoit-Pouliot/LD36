782095b38af42627b6cb93a14ff9f02d14dc7ae5
Marie <marie.44.56@hotmail.com>
2016-08-29 20:37:55 -0400

    Balance fixed.

--- app/sprites/enemy/enemyMusicBoss.py
+++ app/sprites/enemy/enemyMusicBoss.py
@@ -25,7 +25,7 @@ class EnemyMusicBoss(Enemy):
         self.isGravityApplied = True
         self.isCollisionApplied = True
 
-        self.life = 8
+        self.life = 6
 
         image1 = self.image
         image2 = pygame.image.load(os.path.join('img', 'enemyMusicBossAnim.png'))
